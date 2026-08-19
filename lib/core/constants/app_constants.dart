33cddaeeaee1c7ec559cd71868bc553fea183131
BIRUK-MULATU <birukmulatukibret@gmail.com>
2026-08-19 15:38:52 +0300

    chang the app name to rdhis2 mobile app

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -2,7 +2,7 @@ class AppConstants {
   AppConstants._();
 
   // ── App Info ───────────────────────────────────────────────
-  static const String appName = 'RDHIS2 Mobile Tracker';
+  static const String appName = 'RDHIS2 Mobile App';
   static const String appVersion = '1.0.0';
 
   // ── Storage Keys ───────────────────────────────────────────
